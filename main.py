92e932ba8b4dea6a65991afab25818c08598ef17
Leo Lundqvist <163914364+leoldqv@users.noreply.github.com>
2024-11-18 11:10:20 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<path:path>')
+def reroute(path):
+    return redirect(os.environ.get('url'), code=302)
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
